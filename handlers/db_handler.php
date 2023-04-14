f2ebc4ffe44f0590374b2ddfbf5c7148d7bf0d74
Jonathon Meney <79815359+OsmiumCoder@users.noreply.github.com>
2023-04-14 12:58:41 -0300

    Update db_handler.php

--- handlers/db_handler.php
+++ handlers/db_handler.php
@@ -1,14 +1,14 @@
 <?php
-// $dbServerName = "peicloud.ca";
-// $dbUsername = "u120";
-// $dbPassword = "u120";
-// $dbName = "db120";
+$dbServerName = "peicloud.ca";
+$dbUsername = "u120";
+$dbPassword = "u120";
+$dbName = "db120";
 
 
-$dbServerName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "character_creator";
+// $dbServerName = "localhost";
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbName = "character_creator";
 
 $db = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
 
